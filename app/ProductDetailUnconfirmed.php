eda484a92bb9d0aec49c62dcc7291a8ddba029f4
ren012345 <2101007@s.asojuku.ac.jp>
2022-10-26 11:09:11 +0900

    作成途中

--- app/ProductDetailUnconfirmed.php
+++ app/ProductDetailUnconfirmed.php
@@ -4,9 +4,30 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+  <link rel="stylesheet" href="./css/style.css" />
   <title>商品詳細購入未確定ページ</title>
 </head>
 <body>
-  
+  <div id="ProductDetailUnconfirmed_syohinmei">
+  <h1 id="ProductDetailUnconfirmed_syohin">商品名</h1>
+  </div>
+<div class="row featurette">
+      <div class="col-md-7 order-md-2">
+      <div id="ProductDetailUnconfirmed_genzaikakaku">現在価格</div>
+        
+        
+        <div>即決価格</div>
+        <input type="submit" value="入札">
+        <input type="submit" value="今すぐ落札">
+        <div>カテゴリ</div>
+        
+      </div>
+      <div id="ProductDetailUnconfirmed_syohingazou">
+        <svg class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 500x500" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#eee"></rect><text x="50%" y="50%" fill="#aaa" dy=".3em">商品画像</text></svg>
+      </div>
+  </div>
+    <div id="ProductDetailUnconfirmed_syohinsestumei">商品説明</div>
+    <script src="/docs/5.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </body>
 </html>
