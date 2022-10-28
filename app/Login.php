59a0847ee88ba3a3f809c522e980fa8f03892307
yuhei12345 <2101024@s.asojuku.ac.jp>
2022-10-28 13:37:39 +0900

    登録画面作成

--- app/Login.php
+++ app/Login.php
@@ -5,6 +5,11 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>ログイン画面</title>
+  <h2>FourCraft</h2>
+  <form action="RegisterCompletion.php"method="post">
+  メールアドレス<input type="text"name=""><br>
+  パスワード　　<input type="text"name="">
+  <p><input type="submit"value="新規登録"></p>
 </head>
 <body>
   
--- app/RegisterCompletion.php
+++ app/RegisterCompletion.php
@@ -6,7 +6,18 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>登録完了画面</title>
 </head>
+<style>
+  .kanryo{
+    font-size:50px;
+  }
+</style>
 <body>
-  
+  <div style="text-align:center;">
+    <img src="logo.png"width="200"height="200">
+    <div class="kanryo">
+      登録完了しました
+    </div>
+    <a href="ProductList.php">商品一覧画面へ</a>
+  </div>
 </body>
 </html>
