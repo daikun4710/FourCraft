1784e07cbbeb3d7bcc20a7a6a3b6fb3380951cdb
1GiTHuB9 <a04719564@gmail.com>
2022-10-28 12:19:49 +0900

    header上固定

--- app/header/headerlogin.php
+++ app/header/headerlogin.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>ヘッダー_PCログイン</title>
+    <title>ヘッダー_ログイン</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
     <style>
                 /* 992px以上の時に適用 */
@@ -53,8 +53,8 @@
         </div>
         </div>
     </header><!-- ↑ ヘッダー -->
-    
-    <div style="height: 1800px;"></div>
+
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.min.js" integrity="sha384-IDwe1+LCz02ROU9k972gdyvl+AESN10+x7tBKgc9I5HFtuNz0wWnPclzo6p9vxnk" crossorigin="anonymous"></script>
 </body>
 </html>
