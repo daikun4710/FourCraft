634995b896180e96d432e88ce6519a04d2762daa
daikun4710 <tateishidaiki413@gmail.com>
2022-12-08 22:52:53 +0900

    揃え

--- app/ProductList.php
+++ app/ProductList.php
@@ -114,7 +114,7 @@
   </section>
 
   <div class="album py-5 bg-light">
-    <div class="container">
+    <div class="container" style="margin-top: 50px;">
 
       <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
         
